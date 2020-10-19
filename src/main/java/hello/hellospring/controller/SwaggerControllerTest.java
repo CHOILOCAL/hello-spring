de8d126c1d7569d321af486ca44bcdff35b5984c
Choilocal <hyunchoi@hist.co.kr>
2020-10-19 10:12:06 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/controller/SwaggerControllerTest.java
@@ -0,0 +1,35 @@
+package hello.hellospring.controller;
+
+import io.swagger.annotations.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Api(value = "SwaggerControllerTest V1")
+@RequestMapping("/v1/api")
+public class SwaggerControllerTest {
+
+    @ApiOperation(value = "Test", notes = "Swagger API 예제")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "success"),
+            @ApiResponse(code = 400, message = "fail"),
+            @ApiResponse(code = 500, message = "error")
+    })
+    @GetMapping(value = "/test")
+    public Map<String, String> selectTestCode(
+            @ApiParam(value = "번호", required = true, example = "1")
+            @RequestParam String no) {
+
+        Map<String, String> result = new HashMap<>();
+        result.put("name", "choilocal");
+        result.put("age", "24");
+
+        return result;
+    }
+
+}
