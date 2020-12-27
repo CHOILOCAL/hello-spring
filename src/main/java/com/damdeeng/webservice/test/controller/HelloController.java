e2988b2e3e179e7ccece08c28efb56ab59eb9a76
Choilocal <hyunchoi@hist.co.kr>
2020-12-27 21:17:36 +0900

    dto test 추가

--- src/main/java/com/damdeeng/webservice/test/controller/HelloController.java
+++ src/main/java/com/damdeeng/webservice/test/controller/HelloController.java
@@ -1,8 +1,10 @@
 package com.damdeeng.webservice.test.controller;
 
+import com.damdeeng.webservice.test.dto.HelloResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,6 +19,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 
 }
