330af29d3463bdb1abd0beab55694ab0ed6faf2b
choilocal <hyunchoi@hist.co.kr>
2020-12-28 17:16:48 +0900

    mustache 템플릿 엔진 추가

---
+++ src/main/java/com/damdeeng/webservice/test/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.damdeeng.webservice.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(value = "/index")
+    public String index() {
+        return "index";
+    }
+}
